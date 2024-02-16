299a71c1f487a3d615b82a731ed23e6f2d0bee7d
xiyuan <xiyuan230@qq.com>
2024-02-17 00:03:38 +0800

    fix: 修复参赛报名表update无ID的bug

--- src/main/java/com/xiyuan/project/model/dto/competitionentry/CompetitionEntryUpdateRequest.java
+++ src/main/java/com/xiyuan/project/model/dto/competitionentry/CompetitionEntryUpdateRequest.java
@@ -1,6 +1,8 @@
 package com.xiyuan.project.model.dto.competitionentry;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,6 +13,10 @@ import java.io.Serializable;
  */
 @Data
 public class CompetitionEntryUpdateRequest implements Serializable {
+    /**
+     * ID
+     */
+    private Long id;
 
     /**
      * 0-未获奖 1-三等奖 2-二等奖 3-一等奖
